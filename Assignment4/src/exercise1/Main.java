ef8bd595350fa939c05d0c0456801764c65f7ca8
CheTuoi <59533472+CheTuoi@users.noreply.github.com>
2021-11-26 21:20:46 +0700

    Update Main.java

--- Assignment4/src/exercise1/Main.java
+++ Assignment4/src/exercise1/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Student obj = new Student();
-        obj.ss = "Freshee";
+        obj.ss = "Freshee.hihi";
         System.out.println(obj.ss);
         System.out.println(obj.name);
 	}
